b3e282f346d48dfb6e9927eadc61bb8443fc1a48
Junwei Zhang <zjunweihit@163.com>
2019-09-20 16:58:24 +0800

    basic: print info

---
+++ basic/print.py
@@ -0,0 +1,13 @@
+
+# 0.333
+print('{0:.3f}'.format(1/3))
+
+# extend to 11 width
+print('{0:_^11}'.format('hello'))
+
+# use key word to print the info
+print('{name} programed the {code}'.format(name='zjunwei', code='pyLab'))
+
+# show the info in one line
+print('line1:', end='')
+print('one')
